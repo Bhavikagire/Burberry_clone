be366a336a512906c9457de3c2892ec26043112a
gitusergb <gauribidwai94@gmail.com>
2023-01-21 18:32:30 +0530

    order done

---
+++ scripts/orders.js
@@ -0,0 +1,75 @@
+const getOrdersData = async () => {
+    let res = await fetch(`https://bb-nwfw.onrender.com/order_details`);
+    let data = await res.json();
+    appendOrdersData(data);
+  };
+  getOrdersData();
+
+ 
+  
+  const appendOrdersData = (data) => {
+    let orders_div = document.getElementById("orders_tbody");
+    orders_div.innerHTML = "";
+    data.forEach(({ id, images,Pimages,title,dec, price, Ptype, qty }) => {
+      let tr = document.createElement("tr");
+  
+      let img_td = document.createElement("td");
+      let img = document.createElement("img");
+      img.className = "pic"
+      img.src =images[1];
+      img_td.append(img);
+  
+      let pimg_td = document.createElement("td");
+      let pimg = document.createElement("img");
+      pimg.className = "pic2"
+      pimg.src = Pimages[1];
+      pimg_td.append(pimg);
+  
+      let name = document.createElement("td");
+      name.innerText = title;
+  
+      let d_span = document.createElement("span");
+      d_span.innerText = dec;
+      let description = document.createElement("td");
+      description.append(d_span);
+      
+  
+      let pri = document.createElement("td");
+      pri.innerText =price;
+     
+  
+      let Type = document.createElement("td");
+     Type.innerText = Ptype;
+  
+      let inventory = document.createElement("td");
+      inventory.innerText = qty;
+  
+    
+  
+  
+      // <i class="fa-solid fa-trash-can"></i>
+      let del = document.createElement("td");
+      let del_icon = document.createElement("i");
+      del_icon.classList.add("fa-solid", "fa-trash-can", "del_icon");
+      del.append(del_icon);
+      del_icon.onclick = (e) => {
+        if (confirm("Press Ok! to Remove")) {
+          console.log("OK!");
+          removeProduct(id);
+          e.target.parentNode.parentNode.remove();
+        }
+      };
+  
+      tr.append(img_td, name, pri,Type,inventory, del);
+        orders_div.append(tr);
+      }
+    );
+  };
+
+  //remove Products
+const removeProduct = async (id) => {
+  let res = await fetch(`https://bb-nwfw.onrender.com/order_details/${id}`, {
+    method: "DELETE",
+  });
+  alert("Product Deleted!");
+};
