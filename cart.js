75850abe177e6d81cef626ad79e58f87dba7b71a
Thite Shubham <115460300+ThiteShubham@users.noreply.github.com>
2023-01-23 09:58:38 +0530

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,5 +1,6 @@
 
 
+
 let cartdata = JSON.parse(localStorage.getItem('cart')) || []
 
 let product_dom = document.getElementById('product-dom')
@@ -28,8 +29,28 @@ function renderDom(data){
         title.innerText=ele.title
         let price = document.createElement('p')
         price.innerText=`$ ${ele.price}`
+        let inc = document.createElement('button')
+        inc.innerText='+'
+        inc.setAttribute("class", "incButton")
+        inc.addEventListener('click',()=>{
+            ele.qty++
+            localStorage.setItem('cart',JSON.stringify(cartdata))
+            renderDom(cartdata)
+            priceDom(cartdata)
+        })
+        let span=document.createElement('span')
+        span.innerText=ele.qty
+        let dec = document.createElement('button')
+        dec.innerText='-'
+        dec.addEventListener('click',()=>{
+            ele.qty--
+            localStorage.setItem('cart',JSON.stringify(cartdata))
+            renderDom(cartdata)
+            priceDom(cartdata)
+        })
         let del = document.createElement('button')
         del.innerText='Remove'
+        del.setAttribute("class", "delButton")
         del.addEventListener('click',()=>{
             let con = confirm('Do you want to Remove Item')
             if(con){
@@ -40,21 +61,22 @@ function renderDom(data){
                 alert("product deleted");
             }
         })
-        ddiv.append(title,price,del)
-
+        ddiv.append(title,price,inc,span,dec,del)
 
         div.append(idiv,ddiv)
         product_dom.append(div)
     })
 }
 
+document.getElementById('paypal').addEventListener('click',()=>{
+    location.href="https://google.com"
+})
 
 document.getElementById('checkout').addEventListener('click',()=>{
     console.log('yes');
     localStorage.setItem('loder',JSON.stringify('payment.html'))
     location.href="./loder.html"
 })
-
 
 function priceDom(data){
     let total=0;
@@ -67,4 +89,6 @@ function priceDom(data){
     document.getElementById('tota').innerText=total
     document.getElementById('span').innerText=count
 }
+
+
 
